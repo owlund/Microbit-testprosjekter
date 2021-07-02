dac46a2aa247fb6ca5d815020a91f0b090bf0c84
owlund <36522255+owlund@users.noreply.github.com>
2021-07-02 11:08:44 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(500)
+})
